6cea3ccc1e42503ca6080b08e23ab4f44aa31c2b
testy-food <105077621+testy-food@users.noreply.github.com>
2022-05-06 22:41:23 +0200

    change1

---
+++ assets/pages/contact/assets/js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+    // responsive navbar
+
+    $("#bars").on("click", function () {
+
+        $("#bars").toggleClass("fa-times")
+        $(".navbar").toggleClass("active")
+
+    })
+
+    // [mobile view] close navbar when scroll
+
+    $(window).on("scroll", function () {
+
+        $("#bars").removeClass("fa-times")
+        $(".navbar").removeClass("active")
+
+    })
+
+    // open and close search view
+
+    $("#search-icon").on("click", function() {
+
+        $("#search-form").toggleClass("active")
+
+    })
+
+    $("#close").on("click", function() {
+
+        $("#search-form").removeClass("active")
+
+    })
+
+    // loading screen 
+
+    $(".loader").fadeOut("slow")
+
+    // add dark class to body
+
+    $(".fa-moon").on("click", function() {
+
+        $("body").toggleClass("dark")
+        $(this).toggleClass("active")
+
+    })
+
+})
